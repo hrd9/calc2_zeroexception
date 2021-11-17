45ac0429f26f06a1414b9ac4d76e1669b8912479
hrd9 <hrd9@njit.edu>
2021-11-16 21:46:30 -0500

    Improvized the code by defining the separate method for division zero exception

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -8,11 +8,8 @@ def test_calculator_result():
 
 def test_calculator_add():
     """Testing the Add function of the calculator"""
-    #Arrange by instantiating the calc class
     calc = Calculator()
-    #Act by calling the method to be tested
     calc.add_number(4)
-    #Assert that the results are correct
     assert calc.result == 4
 
 def test_calculator_get_result():
@@ -25,13 +22,20 @@ def test_calculator_subtract():
     calc = Calculator()
     calc.subtract_number(1)
     assert calc.get_result() == -1
+
 def test_calculator_multiply():
     """ tests multiplication of two numbers"""
     calc = Calculator()
     result  = calc.multiply_numbers(1,2)
     assert result == 2
+
 def test_calculator_divide():
     """ tests divide of two numbers"""
     calc = Calculator()
-    result  = calc.divide_numbers(1,0)
-    assert result == 0.5
+    result = calc.divide_numbers(2,2)
+    assert result == 1
+
+def test_zeroexception():
+    """Testing the zero exception logic"""
+    calc = Calculator()
+    calc.divide_numbers(3,0)
